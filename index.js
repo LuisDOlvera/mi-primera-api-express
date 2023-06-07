2d005949dcfffa09d4e88d9cd432e5af9e26fda5
LuisDOlvera <luisd_84@hotmail.com>
2023-06-06 20:43:14 -0600

    Create API with express, Json Koders and Mentors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-//Aplicación con Express
 const express = require("express");
 const app = express();
 const fsPromises = require("fs/promises");
+const { join } = require("path");
 
 // Endpoint de Home
 app.get("/", (req, res) => {
@@ -19,20 +19,44 @@ app.get("/", (req, res) => {
  *
  */
 
+/**
+ * - En el lado de backend -
+ * Path params -> modifican la ruta agregando :pathParam
+ * Query params -> no modifican la ruta
+ * 
+ * - En el lado del cliente(google chrome, insomnia, postman);
+ * Path params -> /recurso/pathParam/recurso/pathParam
+ * Query Params -> /recurso?pathParam=valor 
+ *    ? -> vamos a poner query params
+ *    & -> concatenamos query params
+ */
+
+
 // List koders
 app.get("/koders", async (req, res) => {
+    const { module } = req.query;
+    console.log("req.query", req.query)
     const db = await fsPromises.readFile("./koders.json", "utf8"); // leemos base de datos
     const parsedDB = JSON.parse(db); // parseamos json
-    console.log("DB no parseada", db);
-    console.log("DB si parseada", parsedDB);
-    res.json(parsedDB); // respondemos con header de Content-Type -> application/json
+    const filteredKoders = parsedDB.koders.filter(koder => module === koder.module)
+    // Esta vacio, no filtro
+    if(filteredKoders.length === 0) {
+        res.json(parsedDB.koders)
+    } else {
+        res.json(filteredKoders); // respondemos con header de Content-Type -> application/json
+    }
 });
 
 // Get Koder by name
 /**
  * Query params ->>>> ?modulo=Backend
  * Path params(identificador) ->>>> :name
+ * 
+ * ?postId=2
  */
+
+
+//List Koders
 app.get("/koders/:name", async (req, res) => {
     // Path param
     const { name } = req.params;
@@ -44,7 +68,40 @@ app.get("/koders/:name", async (req, res) => {
     res.json(filteredKoder);
 });
 
-//Prendemos nuestro servidor
+// List mentors (age) (Arquitectura rest)
+app.get("/mentors", async (req, res) => {
+  const { age } = req.query;
+  console.log("req.query", req.query)
+  const db = await fsPromises.readFile("./koders.json", "utf8"); // leemos base de datos
+  const parsedDB = JSON.parse(db); // parseamos json
+  const filteredMentors = parsedDB.mentors.filter(mentor => mentor.age === age)
+
+/**
+     * 1 -> quiere filtrar y encontro filtrado
+     * 2 -> quiere filtrar y no encontro filtrado
+     * 3 -> no quiere filtrar, quiere todos los mentores
+     */
+
+  //Quiere filtrar y el arreglo no está vacío
+  if(age && filteredMentors.length > 0) {
+      res.json(filteredMentors)
+  //No quiere filtrar    
+  } else if (!age) {
+      res.json(parsedDB.mentors); // respondemos con header de Content-Type -> application/json
+  //Quiere filtrar pero no encontró un mentor    
+  } else {
+    res.json({message: "El mentor no fue encontrado"});
+  }
+});
+
 app.listen(8080, () => {
     console.log("Nuestro servidor esta prendido");
 });
+
+
+/**
+ * Ejercicio
+ * 2 endpoints -
+ * 1.er Donde me filtren por age los mentores
+ * 2.do Donde obtengamos un mentor en especifico con su nombre
+ */
